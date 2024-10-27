c12ee06bd055dc4fdc057bc2e2e9853ab6792aa3
Milton Pereira <miltonrpg@gmail.com>
2024-10-27 21:28:39 +0100

    Cambios finales

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,18 +4,19 @@ const app = require('../app');
 let server;
 
 beforeAll(() => {
-    server = app.listen(3000);
+    server = app.listen(0);
 });
 
-afterAll(() => {
-    server.close();
+afterAll((done) => {
+    server.close(done); // Asegura el cierre completo del servidor
 });
 
 describe('GET /', () => {
     it('responds with Hello, CI/CD World!', async () => {
-        const response = await request(app).get('/');
+        const response = await request(server).get('/'); // Cambia `app` por `server`
         expect(response.status).toBe(200);
         expect(response.text).toBe('Hello, CI/CD World!');
     });
 });
+
 
